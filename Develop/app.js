92ab713c664f46c0418fded7fe2a7bf970eaf13b
Dr Dougie <aaron.m.larsh@gmail.com>
2020-07-26 02:03:52 -0700

    minor fixes

--- Develop/app.js
+++ Develop/app.js
@@ -117,13 +117,10 @@ async function start(){
             break;
         }
     } 
-    console.log(teamObj)
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 let htmlTeamList = render(teamObj);
-console.log(htmlTeamList);
-
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
@@ -136,18 +133,6 @@ fs.writeFile(outputPath, htmlTeamList, (err) => {
         console.log('Finished.')
     }
 });
-
-
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
 }
 start();
 
