52fc16bd7baeaee78e0f2a219837ea1e7e7636a1
Dr Dougie <aaron.m.larsh@gmail.com>
2020-07-25 23:42:46 -0700

    finished product

--- Develop/app.js
+++ Develop/app.js
@@ -129,6 +129,15 @@ console.log(htmlTeamList);
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+fs.writeFile(outputPath, htmlTeamList, (err) => {
+    if(err){
+        console.log(err);
+    }else {
+        console.log('Finished.')
+    }
+});
+
+
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
